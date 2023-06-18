9bc621bc4981ebc3a11ce9d926525cd4a22a565c
copterbuddy <copterbuddy@gmail.com>
2023-06-18 17:53:27 +0700

    RemoveElement

---
+++ NeetCodeExam.Test/1.StaticArray/2.RemoveElement.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeetCodeExam.Test.StaticArray;
+public class RemoveElementTest
+{
+    [Fact]
+    public void TestRemoveElementTest_Success_Case1()
+    {
+        int[] nums = new int[] { 3, 2, 2, 3 };
+        int val = 3;
+        int result = Program.RemoveElement(nums, val);
+        Assert.Equal(2, result);
+    }
+    [Fact]
+    public void TestRemoveElementTest_Success_Case2()
+    {
+        int[] nums = new int[] { 0, 1, 2, 2, 3, 0, 4, 2 };
+        int val = 2;
+        int result = Program.RemoveElement(nums, val);
+        Assert.Equal(5, result);
+    }
+}
---
+++ NeetCodeExam/1.StaticArray/2.RemoveElement.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeetCodeExam;
+partial class Program
+{
+    public static int RemoveElement(int[] nums, int val)
+    {
+        int[] newnums = new int[nums.Length];
+        int l = 0;
+
+        for (int r = 0; r < nums.Length; r++)
+        {
+            if (nums[r] == val)
+            {
+                continue;
+            }
+
+            nums[l++] = nums[r];
+        }
+
+        return l;
+    }
+}
