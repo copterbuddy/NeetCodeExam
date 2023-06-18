c5b4fa43b9c11fe80725fc27913fb1fb383e7c9b
copterbuddy <copterbuddy@gmail.com>
2023-06-18 20:35:37 +0700

    ConcatenationOfArray

---
+++ NeetCodeExam.Test/1.Arrays/4.DynamicArrays/1.ConcatenationOfArray.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeetCodeExam.Test.DynamicArrays.Arrays;
+public class ConcatenationOfArray
+{
+    [Fact]
+    public void TestConcatenationOfArray_Success_Case1()
+    {
+        int[] nums = new int[] { 1, 2, 1 };
+        int[] result = Program.GetConcatenation(nums);
+        Assert.Equal(new int[] { 1, 2, 1, 1, 2, 1 }, result);
+    }
+
+    [Fact]
+    public void TestConcatenationOfArray_Success_Case2()
+    {
+        int[] nums = new int[] { 1, 3, 2, 1 };
+        int[] result = Program.GetConcatenation(nums);
+        Assert.Equal(new int[] { 1, 3, 2, 1, 1, 3, 2, 1 }, result);
+    }
+}
---
+++ NeetCodeExam/1.Arrays/4.DynamicArrays/1.ConcatenationOfArray.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeetCodeExam;
+partial class Program
+{
+    public static int[] GetConcatenation(int[] nums)
+    {
+        int[] newnums = new int[nums.Length*2];
+
+        for (int i = 0; i < nums.Length * 2; i++)
+        {
+            int index = i;
+            if (i > nums.Length-1)
+            {
+                index = i - (nums.Length);
+            }
+
+            newnums[i] = nums[index];
+        }
+
+        return newnums;
+    }
+}
