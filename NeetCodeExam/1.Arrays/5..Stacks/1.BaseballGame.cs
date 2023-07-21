8486ba2f11374e97a885a15d61f9c7d2407cfcbb
copterbuddy <copterbuddy@gmail.com>
2023-07-21 15:39:08 +0700

    change to use new param name stack
    because we have to foreach to last index

--- NeetCodeExam/1.Arrays/5..Stacks/1.BaseballGame.cs
+++ NeetCodeExam/1.Arrays/5..Stacks/1.BaseballGame.cs
@@ -9,29 +9,30 @@ partial class Program
 {
     public static int CalPoints(string[] operations) {
         int index = 0;
+        string[] stacks = new string[operations.Length];
         foreach (var item in operations)
         {
             int number = 0;
             bool IsNumber = Int32.TryParse(item, out number);
             if (IsNumber)
             {
-                operations[index] = item;
+                stacks[index] = item;
             }
             else
             {
                 if (item == "C")
                 {
-                    operations[index-1] = null;
-                    operations[index] = null;
+                    stacks[index-1] = null;
+                    stacks[index] = null;
                 }
             }
             index++;
         }
         
         int sum = 0;
-        for (int i = 0; i < operations.Length; i++)
+        for (int i = 0; i < stacks.Length; i++)
         {
-            if (int.TryParse(operations[i], out int num))
+            if (int.TryParse(stacks[i], out int num))
             {
                 sum += num;
             }
