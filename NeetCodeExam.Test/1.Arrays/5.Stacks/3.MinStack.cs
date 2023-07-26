298de47b37e5b8ba5588768efee5a3ab3d16dc46
copterbuddy <copterbuddy@gmail.com>
2023-07-26 19:56:56 +0700

    stacks - minStack

---
+++ NeetCodeExam.Test/1.Arrays/5.Stacks/3.MinStack.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeetCodeExam.Test.Stacks.MinStack;
+
+public class TestMinStack
+{
+    [Fact]
+    public void TestTestMinStack_Success_Case1()
+    {
+        Program.MinStack minStack = new Program.MinStack();
+        minStack.Push(-2);
+        minStack.Push(0);
+        minStack.Push(-3);
+        minStack.GetMin(); // return -3
+        minStack.Pop();
+        minStack.Top();    // return 0
+        minStack.GetMin(); // return -2
+    }
+
+    [Fact]
+    public void TestTestMinStack_Success_Case2()
+    {
+        Program.MinStack minStack = new Program.MinStack();
+        minStack.Push(2);
+        minStack.Push(0);
+        minStack.Push(3);
+        minStack.Push(0);
+        minStack.GetMin(); // return 0
+        minStack.Pop();
+        minStack.GetMin(); // return 0
+        minStack.Pop();
+        minStack.GetMin(); // return 0
+        minStack.Pop();
+        minStack.GetMin(); // return 2
+    }
+}
---
+++ NeetCodeExam/1.Arrays/5.Stacks/3.MinStack.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static NeetCodeExam.Program;
+
+namespace NeetCodeExam;
+
+partial class Program
+{
+    public class MinStack
+    {
+        public Stack<int> stack;
+        public Stack<int> minStack;
+
+        public MinStack()
+        {
+            stack = new Stack<int>();
+            minStack = new Stack<int>();
+        }
+
+        public void Push(int val)
+        {
+            stack.Push(val);
+            if (minStack.Count() == 0 || val <= minStack.Peek())
+            {
+                minStack.Push(val);
+            }
+            else
+            {
+                minStack.Push(minStack.Peek());
+            }
+        }
+
+        public void Pop()
+        {
+            stack.Pop();
+            minStack.Pop();
+        }
+
+        public int Top()
+        {
+            return stack.Peek();
+        }
+
+        public int GetMin()
+        {
+            return minStack.Peek();
+        }
+    }
+
+}
