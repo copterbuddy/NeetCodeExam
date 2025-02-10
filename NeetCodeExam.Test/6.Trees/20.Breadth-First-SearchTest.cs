a88bc4b5690236ca99818d454e724818b91c3ed1
copterbuddy <copterbuddy@gmail.com>
2025-02-10 19:48:20 +0700

    RightSideView_DepthFirst

--- NeetCodeExam.Test/6.Trees/20.Breadth-First-SearchTest.cs
+++ NeetCodeExam.Test/6.Trees/20.Breadth-First-SearchTest.cs
@@ -33,4 +33,34 @@ public class Breadth_First_SearchTest
         List<int> result = app.RightSideView(input);
         Assert.Equivalent(want, result);
     }
+
+    [Fact]
+    public async Task Test_RightSideView_DepthFirst_Case1()
+    {
+        TreeNode input = new(val: 1,
+            left: new(val: 2),
+            right: new (val: 3));
+
+        List<int> want = [1,3];
+        
+        List<int> result = app.RightSideView_DepthFirst(input);
+        Assert.Equivalent(want, result);
+    }
+
+    [Fact]
+    public async Task Test_RightSideView_DepthFirst_Case2()
+    {
+        TreeNode input = new(val: 1,
+            left: new(val: 2,
+                left: new(val: 4),
+                right: new(val: 5)),
+            right: new (val: 3,
+                left: new(val: 6),
+                right: new(val: 7)));
+
+        List<int> want = [1,3,7];
+        
+        List<int> result = app.RightSideView_DepthFirst(input);
+        Assert.Equivalent(want, result);
+    }
 }
--- NeetCodeExam/6.Trees/20.Breadth-First-Search.cs
+++ NeetCodeExam/6.Trees/20.Breadth-First-Search.cs
@@ -63,4 +63,24 @@ public class Breadth_First_Search {
 
         return result;
     }
+
+    List<int> res = new List<int>();
+    
+    public List<int> RightSideView_DepthFirst(TreeNode root) {
+        dfs(root, 0);
+        return res;
+    }
+    
+    private void dfs(TreeNode node, int depth) {
+        if (node == null) {
+            return;
+        }
+        
+        if (res.Count == depth) {
+            res.Add(node.val);
+        }
+        
+        dfs(node.right, depth + 1);
+        dfs(node.left, depth + 1);
+    }
 }
