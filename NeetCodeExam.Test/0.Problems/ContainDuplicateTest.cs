c6a4c7f417fe29d8f4cfeecb577412047ce1431d
copterbuddy <copterbuddy@gmail.com>
2025-02-15 16:37:39 +0700

    contain duplicate

---
+++ NeetCodeExam.Test/0.Problems/ContainDuplicateTest.cs
@@ -0,0 +1,22 @@
+namespace NeetCodeExam.Problems;
+
+public class ContainDuplicateTest
+{
+    ContainDuplicate app = new();
+
+    [Fact]
+    public void TestContainDuplicate_Want_True_Success_Case1()
+    {
+        int[] nums = [1, 2, 3, 1];
+        bool result = app.ContainsDuplicate(nums);
+        Assert.False(result);
+    }
+
+    [Fact]
+    public void TestContainDuplicate_Want_False_Success_Case1()
+    {
+        int[] nums = [1, 2, 3, 4];
+        bool result = app.ContainsDuplicate(nums);
+        Assert.True(result);
+    }
+}
---
+++ NeetCodeExam/0.Problems/ContainDuplicate.cs
@@ -0,0 +1,20 @@
+namespace NeetCodeExam.Problems;
+
+public class ContainDuplicate
+{
+    public bool ContainsDuplicate(int[] nums)
+    {
+        Dictionary<int, byte> result = new();
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (result.ContainsKey(nums[i]) == false)
+            {
+                result.Add(nums[i], byte.MinValue);
+                continue;
+            }
+
+            return false;
+        }
+        return true;
+    }
+}
