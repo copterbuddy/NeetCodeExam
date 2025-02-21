a2755e9e27c288436d172b39555ff886d46d6e70
copterbuddy <copterbuddy@gmail.com>
2025-02-21 21:22:23 +0700

    add TopKFrequent

--- NeetCodeExam/0.Problems/TopKFrequents.cs
+++ NeetCodeExam/0.Problems/TopKFrequents.cs
@@ -4,6 +4,22 @@ public class TopKFrequents
 {
     public int[] TopKFrequent(int[] nums, int k)
     {
-        return null;
+        Dictionary<int, int> map = new();
+        for (int i = 0; i < nums.Length; i++)
+        {
+            int value = nums[i];
+            if (map.ContainsKey(value) == false)
+            {
+                map[value] = 1;
+            }
+            else
+            {
+                map[value]++;
+            }
+        }
+
+        var mapSorted = map.Select(m => new { key = m.Key, value = m.Value }).ToArray();
+        Array.Sort(mapSorted, (a, b) => b.value.CompareTo(a.value));
+        return mapSorted.Take(k).Select(x => x.key).ToArray();
     }
 }
