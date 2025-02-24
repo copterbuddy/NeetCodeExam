c496ef56e3e54f39a518e1d0c81f42b987cbafab
copterbuddy <copterbuddy@gmail.com>
2025-02-24 20:54:38 +0700

    add IsValidSudoku

--- NeetCodeExam/0.Problems/ValidSudoku.cs
+++ NeetCodeExam/0.Problems/ValidSudoku.cs
@@ -4,6 +4,55 @@ public class ValidSudoku
 {
     public bool IsValidSudoku(char[][] board)
     {
+        Dictionary<int, Dictionary<char, byte>> row = new();
+        Dictionary<int, Dictionary<char, byte>> col = new();
+        Dictionary<Tuple<int, int>, Dictionary<char, byte>> sqr = new();
+
+        for (int c = 0; c < board[0].Length; c++)
+        {
+            row.Add(c, new());
+
+        }
+
+        for (int r = 0; r < board.Length; r++)
+        {
+            col.Add(r, new());
+        }
+
+        for (int r = 0; r < board.Length; r++)
+        {
+            for (int c = 0; c < board[0].Length; c++)
+            {
+                var tuple = new Tuple<int, int>(r / 3, c / 3);
+                if (sqr.ContainsKey(tuple) == false)
+                {
+                    sqr.Add(tuple, new());
+                }
+            }
+        }
+
+        for (int r = 0; r < board.Length; r++)
+        {
+            for (int c = 0; c < board[0].Length; c++)
+            {
+                if (board[r][c] == '.')
+                {
+                    continue;
+                }
+
+                if (row[c].ContainsKey(board[r][c]) ||
+                col[r].ContainsKey(board[r][c]) ||
+                sqr[new Tuple<int, int>(r / 3, c / 3)].ContainsKey(board[r][c]))
+                {
+                    return false;
+                }
+
+                row[c].Add(board[r][c], 1);
+                col[r].Add(board[r][c], 1);
+                sqr[new Tuple<int, int>(r / 3, c / 3)].Add(board[r][c], 1);
+            }
+        }
+
         return true;
     }
 }
