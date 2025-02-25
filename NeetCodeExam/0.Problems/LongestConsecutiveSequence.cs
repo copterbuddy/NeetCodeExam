e7a201205fb1e014fb6140528177ef7a3ca00e03
copterbuddy <copterbuddy@gmail.com>
2025-02-25 20:16:29 +0700

    add LongestConsecutive

--- NeetCodeExam/0.Problems/LongestConsecutiveSequence.cs
+++ NeetCodeExam/0.Problems/LongestConsecutiveSequence.cs
@@ -4,6 +4,24 @@ public class LongestConsecutiveSequence
 {
     public int LongestConsecutive(int[] nums)
     {
-        return 0;
+        Dictionary<int, int> map = nums.Distinct().ToDictionary((k) => k, (v) => v);
+        Dictionary<int, int> result = new();
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            int length = 0;
+            int prev = nums[i];
+            while (map.ContainsKey(prev))
+            {
+                length++;
+                prev++;
+                result[nums[i]] = length;
+            }
+        }
+
+        var arrResult = result.Select(x => new { key = x.Key, value = x.Value }).ToArray();
+        var arrSorted = arrResult.OrderByDescending(x => x.value).ToArray();
+        var maxData = arrSorted.FirstOrDefault().value;
+        return maxData;
     }
 }
--- NeetCodeExam/0.Problems/ValidSudoku.cs
+++ NeetCodeExam/0.Problems/ValidSudoku.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+
 namespace NeetCodeExam.Problems;
 
 public class ValidSudoku
