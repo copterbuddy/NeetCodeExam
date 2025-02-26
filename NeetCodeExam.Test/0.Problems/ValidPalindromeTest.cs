bebf88341297174051767baf62a8045dadcfba28
copterbuddy <copterbuddy@gmail.com>
2025-02-26 20:38:04 +0700

    add IsPalindrome

---
+++ NeetCodeExam.Test/0.Problems/ValidPalindromeTest.cs
@@ -0,0 +1,24 @@
+using System;
+
+namespace NeetCodeExam.Problems;
+
+public class ValidPalindromeTest
+{
+    ValidPalindrome app = new();
+
+    [Fact]
+    public void TestIsAnagram_Want_True_Success_Case1()
+    {
+        string s = "Was it a car or a cat I saw?";
+        bool result = app.IsPalindrome(s);
+        Assert.True(result);
+    }
+
+    [Fact]
+    public void TestIsAnagram_Want_True_Fail_Case1()
+    {
+        string s = "tab a cat";
+        bool result = app.IsPalindrome(s);
+        Assert.False(result);
+    }
+}
---
+++ NeetCodeExam/0.Problems/ValidPalindrome.cs
@@ -0,0 +1,37 @@
+using System;
+
+namespace NeetCodeExam.Problems;
+
+public class ValidPalindrome
+{
+    public bool IsPalindrome(string s)
+    {
+        s = s.ToLower();
+
+        string newS = string.Empty;
+        for (int i = 0; i < s.Length; i++)
+        {
+            var c = s[i];
+            if (c < 'a' || c > 'z')
+            {
+                continue;
+            }
+            newS += c;
+        }
+
+        int left = 0;
+        int right = newS.Length - 1;
+
+        while (left != right)
+        {
+            if (newS[left] != newS[right])
+            {
+                return false;
+            }
+
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
