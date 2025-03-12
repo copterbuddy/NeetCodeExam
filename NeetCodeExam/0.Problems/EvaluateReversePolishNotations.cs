d7e608acc1791c0f3e4897f96888dd56e4c73115
copterbuddy <copterbuddy@gmail.com>
2025-03-12 22:27:13 +0700

    add EvalRPN

--- NeetCodeExam/0.Problems/EvaluateReversePolishNotations.cs
+++ NeetCodeExam/0.Problems/EvaluateReversePolishNotations.cs
@@ -4,6 +4,38 @@ public class EvaluateReversePolishNotations
 {
     public int EvalRPN(string[] tokens)
     {
-        return 0;
+        Stack<int> stack = new();
+        foreach (var token in tokens)
+        {
+            int sum;
+            int a;
+            int b;
+            switch (token)
+            {
+                case "+":
+                    sum = stack.Pop() + stack.Pop();
+                    stack.Push(sum);
+                    break;
+                case "-":
+                    a = stack.Pop();
+                    b = stack.Pop();
+                    stack.Push(b - a);
+                    break;
+                case "*":
+                    sum = stack.Pop() * stack.Pop();
+                    stack.Push(sum);
+                    break;
+                case "/":
+                    a = stack.Pop();
+                    b = stack.Pop();
+                    stack.Push(b / a);
+                    break;
+                default:
+                    stack.Push(Convert.ToInt16(token));
+                    break;
+            }
+        }
+
+        return stack.Pop();
     }
 }
