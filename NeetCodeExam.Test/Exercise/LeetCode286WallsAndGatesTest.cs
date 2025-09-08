77a1645ed11ae1269d7191c8f9485a8f157907f9
copterbuddy <copterbuddy@gmail.com>
2025-09-08 21:45:38 +0700

    implements leetcode286

---
+++ NeetCodeExam.Test/Exercise/LeetCode286WallsAndGatesTest.cs
@@ -0,0 +1,28 @@
+﻿using NeetCodeExam.Exercise;
+
+namespace NeetCodeExam.Test.Exercise;
+
+public class LeetCode286WallsAndGatesTest
+{
+    private LeetCode286WallsAndGates app = new();
+
+    [Fact]
+    public async Task Test_Case1()
+    {
+        int maxInt = 2_147_483_647;
+        int[][] give = [
+                      [maxInt, -1,0,maxInt],
+                      [maxInt,maxInt,maxInt,-1],
+                      [maxInt,-1,maxInt,-1],
+                      [0,-1,maxInt,maxInt]
+                    ];
+        app.islandsAndTreasure(give);
+        int[][] want = [
+                      [3,-1,0,1],
+                      [2,2,1,-1],
+                      [1,-1,2,-1],
+                      [0,-1,3,4]
+                    ];
+        Assert.Equal(want, give);
+    }
+}
---
+++ NeetCodeExam/Exercise/LeetCode286WallsAndGates.cs
@@ -0,0 +1,69 @@
+﻿namespace NeetCodeExam.Exercise
+{
+    public class LeetCode286WallsAndGates
+    {
+        public void islandsAndTreasure(int[][] grid)
+        {
+            //int maxInt = 2_147_483_647;
+            int row = grid.Length;
+            int col = grid[0].Length;
+            Queue<int[]> q = new Queue<int[]>();
+            HashSet<string> visited = new HashSet<string>();
+
+            for (int r = 0; r < row; r++)
+            {
+                for (int c = 0; c < col; c++)
+                {
+                    if (grid[r][c] == 0)
+                    {
+                        visited.Add($"{r}{c}");
+                        int[] chrest = new int[] { r, c };
+                        q.Enqueue(chrest);
+                    }
+                }
+            }
+
+            if (q.Count == 0)
+            {
+                return;
+            }
+
+            int[][] directions = new int[][]
+            {
+                new int[] { 1, 0 },
+                new int[] { -1, 0 },
+                new int[] { 0, 1 },
+                new int[] { 0, -1 }
+            };
+
+
+            while (q.Count > 0)
+            {
+                var queue = q.Dequeue();
+                foreach(var d in directions)
+                {
+                    var dr = queue[0] + d[0];
+                    var dc = queue[1] + d[1];
+                    if (
+                        //existing path
+                        dr < 0 ||
+                        dc < 0 ||
+                        dr >= row ||
+                        dc >= col ||
+                        //can't walk path
+                        grid[dr][dc] == -1 ||
+                        //visited
+                        visited.Contains($"{dr}{dc}")
+                        )
+                    {
+                        continue;
+                    }
+
+                    q.Enqueue(new int[] { dr, dc });
+                    visited.Add($"{dr}{dc}");
+                    grid[dr][dc] = grid[queue[0]][queue[1]] + 1;
+                }
+            };
+        }
+    }
+}
