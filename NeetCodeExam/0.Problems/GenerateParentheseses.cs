eec87c8a4d5ad1bc410ceb9ab47945f92da47a38
copterbuddy <copterbuddy@gmail.com>
2026-01-19 19:56:53 +0700

    generate paretheseses

--- NeetCodeExam/0.Problems/GenerateParentheseses.cs
+++ NeetCodeExam/0.Problems/GenerateParentheseses.cs
@@ -1,9 +1,36 @@
+using System.Text;
+
 namespace NeetCodeExam.Problems;
 
 public class GenerateParentheseses
 {
     public List<string> GenerateParenthesis(int n)
     {
-        return null;
+        List<string> result = [];
+        StringBuilder current = new();
+        void Solve(List<string> r,StringBuilder c,int open, int close, int nn)
+        {
+            if(nn == open && open == close)
+            {
+                r.Add(c.ToString());
+                return;
+            }
+
+            if(open < nn)
+            {
+                c.Append('(');
+                Solve(r, c, open + 1, close, nn);
+                c.Length--;
+            }
+
+            if (close < open)
+            {
+                c.Append(')');
+                Solve(r, c, open, close + 1, nn);
+                c.Length--;
+            }
+        }
+        Solve(result, current, 0, 0, n);
+        return result;
     }
 }
